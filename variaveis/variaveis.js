1d88f25f3eb826aab53a7cbb7f43bdc86b132b0c
gaabrieloliver <contato.gabrieloliveira0@gmail.com>
2024-08-23 04:42:09 -0300

    :tada: variaveis let e const

---
+++ variaveis/variaveis.js
@@ -0,0 +1,17 @@
+// console.log('Gabriel Oliveira');
+
+let pessoa = 'Gabriel Oliveira'
+
+console.log(pessoa)
+
+pessoa = 'Fulano'
+
+console.log(pessoa)
+
+const escola = 'Cubos Academy'
+
+console.log(escola)
+
+escola = 'Outra escola'
+
+console.log(escola)
