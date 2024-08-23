a8ed833695ff6d30268e17b61f1b2cd923a0bdfb
gaabrieloliver <contato.gabrieloliveira0@gmail.com>
2024-08-23 05:42:29 -0300

    :tada: operadores de comparação

---
+++ operadores comparação/opcomparacao.js
@@ -0,0 +1,32 @@
+// maior que
+const comparacao1 = 3 > 6
+console.log(comparacao1);
+
+// menor que
+const comparacao2 = 3 < 6
+console.log(comparacao2);
+
+// maior ou igual
+const comparacao3 = 3 >= 6
+console.log(comparacao3);
+
+// menor ou igual
+const comparacao4 = 3 <= 6
+console.log(comparacao4);
+
+// igualdade estrita com tipo de dados iguais
+const comparacao5 = 6 === 6
+console.log(comparacao5);
+
+// igualdade estrita com tipo de dados diferentes
+const comparacao6 = 6 === '6'
+console.log(comparacao6);
+
+// igualdade fraca com tipos de dados diferentes
+const comparacao7 = 6 == '6'
+console.log(comparacao7);
+
+// diferente
+const comparacao8 = 3 != 6
+console.log(comparacao8);
+
