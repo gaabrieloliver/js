2a8d7624a99f944a025966e2893913989b66369b
gaabrieloliver <contato.gabrieloliveira0@gmail.com>
2024-08-23 05:42:43 -0300

    :tada: operadores aritméticos

---
+++ operadores aritméticos/operadores.js
@@ -0,0 +1,15 @@
+// soma
+let soma = 3+5+8+9
+console.log(soma)
+
+// subtração
+let subtracao = 98-8
+console.log(subtracao)
+
+// multiplicação
+let multiplicacao = 4*7
+console.log(multiplicacao)
+
+// divisão
+let divisao = 7/2
+console.log(divisao)
