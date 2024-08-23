497d5d2c7a8909bdf8b14e38d31aaa6f3f3bf2f4
gaabrieloliver <contato.gabrieloliveira0@gmail.com>
2024-08-23 04:43:42 -0300

    :tada: tipos de dados
    String, Number, Arrays, Boolean, Object.

---
+++ tipos de dados/tiposdedados.js
@@ -0,0 +1,20 @@
+// tipo de string (texto)
+let nome = 'Gabriel Oliveira'
+
+// tipo de number (números)
+let idade = '28'
+let altura = '1.70'
+
+// tipo de array (listas)
+// array de strings
+let tecnologias = ['javascript','java','html','css']
+
+// array de numbers
+let numeros = [10, 34, 56, 90]
+
+// array booleano
+let verdadeiro = true
+let falso = false
+
+// tipo de objeto
+const usuario = { nome: 'Gabriel', idade: 28}
