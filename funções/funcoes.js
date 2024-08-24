6a80a791120536be038e073b18531e0e4bdf9e05
gaabrieloliver <contato.gabrieloliveira0@gmail.com>
2024-08-24 07:08:03 -0300

    :tada: exe function
    Exercício de fixação sobre a utilização de funções com parâmetros

---
+++ funções/funcoes.js
@@ -0,0 +1,13 @@
+// forma tradicional
+function imprimir (nome) {
+    console.log(`Olá: ${nome}`);
+}
+
+imprimir('Gabriel')
+
+// função com parâmetro utilizando return
+function somar (n1,n2) {
+   return n1,n2
+}
+
+console.log(somar(50, 30));
