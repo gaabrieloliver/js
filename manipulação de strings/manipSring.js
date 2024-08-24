450109c97f860e3c947eb66c7dbe3978946ed477
gaabrieloliver <contato.gabrieloliveira0@gmail.com>
2024-08-24 07:06:26 -0300

    :tada: add exe manipString
    Manipulação de Strings

---
+++ manipulação de strings/manipSring.js
@@ -0,0 +1,5 @@
+const nome = 'Gabriel Oliveira'
+console.log(nome.toUpperCase())
+console.log(nome.toLowerCase())
+console.log(nome.includes('riel'))
+console.log(nome.replace('Oliveira', 'Barbosa'))
